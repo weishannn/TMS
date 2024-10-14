1f90630a358e8cd227d5227b0bbefaa9cab8ce9b
shanshineeee <kohweishann@gmail.com>
2024-10-14 08:27:55 +0800

    docker

--- backend/models/db.js
+++ backend/models/db.js
@@ -18,26 +18,6 @@ pool.getConnection((err, connection) => {
     return;
   }
   console.log("Connected to the MySQL database");
-
-  // // Perform queries here
-  // connection.query("SELECT * FROM accounts", (err, accounts) => {
-  //   if (err) {
-  //     console.error("Error fetching accounts:", err);
-  //     connection.release();
-  //     return;
-  //   }
-
-  //   console.log("Accounts:", accounts);
-
-  //   connection.query("SELECT * FROM usergroup", (err, userGroups) => {
-  //     if (err) {
-  //       console.error("Error fetching user groups:", err);
-  //     } else {
-  //       console.log("User Groups:", userGroups);
-  //     }
-  //     connection.release();
-  //   });
-  // });
 });
 
 module.exports = pool.promise();
