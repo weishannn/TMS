8eb24ef4ab35b22409aaf58a9526d94b535889fc
shanshineeee <kohweishann@gmail.com>
2024-10-15 15:38:17 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -44,3 +44,20 @@ app.use("/api/demo", demoRoutes);
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+/////testing/////
+// const http = require("http");
+
+// // Define the port to listen on
+// const PORT = process.env.PORT || 3000;
+
+// // Create an HTTP server that responds to all requests
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+//   res.end("Docker container is running\n");
+// });
+
+// // Start the server and listen on the defined port
+// server.listen(PORT, () => {
+//   console.log(`Server running at http://localhost:${PORT}/`);
+// });
